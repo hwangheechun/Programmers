9660d13af599b397f12ab07720cb116c6d600539
hwangheechun <95543384+hwangheechun@users.noreply.github.com>
2025-02-08 00:05:36 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+    
+    int coupon = chicken;
+    
+    while(coupon >= 10)
+    {
+        answer += coupon / 10;
+        coupon = coupon / 10 + coupon % 10;
+    }
+
+    cout << coupon;
+    
+    return answer;
+}
