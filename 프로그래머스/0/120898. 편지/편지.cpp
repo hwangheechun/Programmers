85382db8a2b0cacab4ddfcd2ca8b0e765b41c26a
hwangheechun <95543384+hwangheechun@users.noreply.github.com>
2025-02-21 22:56:56 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    
+    answer = message.size() * 2;
+    
+    return answer;
+}
