9d2829672161a8109bea122f5d93b2214ce93cf3
hwangheechun <95543384+hwangheechun@users.noreply.github.com>
2025-02-26 23:18:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    
+    for (int i = 0; i < my_string.size(); i++)
+    {
+        if( my_string[i] >= '1' && my_string[i] <= '9' )
+        {
+            answer += my_string[i] - '0';   //아스키 코드 차이
+        }
+    }
+    
+    return answer;
+}
